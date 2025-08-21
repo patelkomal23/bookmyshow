9552b53ffc2bf59998c3b4df08e00c7fd61cb5ed
patelkomal2332@gmail.com <patelkomal2332@gmail.com>
2025-08-21 20:55:03 +0530

    donne

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose")
 
 const db = async ()=>{
     try{
-        await mongoose.connect('mongodb+srv://p6579113:12345@cluster0.zlavh0m.mongodb.net/movies')
+        await mongoose.connect('mongodb+srv://patelkomal2332:12345@cluster0.wg4ajfg.mongodb.net/movies')
         console.log("Database Connected ...")
     }
     catch(err){
